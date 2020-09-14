c4682e0f3b8e18bbf8e2826a7e336a5f905f3705
jacoe0850 <jacoe0850@wrdsb.ca>
2020-09-14 15:33:38 +0000

    Finished slope

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  *
  * @author 
@@ -9,7 +10,16 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
-    
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter in the x coordinate of the first point");
+    int x1 = input.nextInt();
+    System.out.println("Please enter in the y coordinate of the first point");
+    int y1 = input.nextInt();
+    System.out.println("Please enter in the x coordinate of the second point");
+    int x2 = input.nextInt();
+    System.out.println("Please enter in the y coordinate of the second point");
+    int y2 = input.nextInt();
+    int slope = (y2 - y1) / (x2 - x1);
+System.out.println("The slope of your line would be " + slope);
   }
 }
